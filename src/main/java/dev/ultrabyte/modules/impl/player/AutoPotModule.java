e87252077ebca31d8caa1041645b021e21f794e9
CNiuRen <25546671@qq.com>
2025-08-06 23:25:11 +0800

    auto pot update & fix

--- src/main/java/dev/ultrabyte/modules/impl/player/AutoPotModule.java
+++ src/main/java/dev/ultrabyte/modules/impl/player/AutoPotModule.java
@@ -10,10 +10,12 @@ import dev.ultrabyte.settings.impl.BooleanSetting;
 import dev.ultrabyte.settings.impl.ModeSetting;
 import dev.ultrabyte.utils.minecraft.InventoryUtils;
 import dev.ultrabyte.utils.minecraft.NetworkUtils;
+import dev.ultrabyte.utils.system.Timer;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Items;
 import net.minecraft.item.PotionItem;
 import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
+import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.util.Hand;
 
 /**
@@ -25,27 +27,31 @@ public class AutoPotModule
 extends Module {
     public final BooleanSetting groundCheck = new BooleanSetting("GroundCheck", "ground check", false);
     public final BooleanSetting resistance = new BooleanSetting("TurtleMaster", "resistance", true);
-    public final BooleanSetting onlyWall = new BooleanSetting("OnlyWall", "resistance", true);
-    public final BooleanSetting stp = new BooleanSetting("StopThrowUp", "stop throw up", true);
+    public final BooleanSetting onlyWall = new BooleanSetting("OnlyWall", "only wall", true);
     public final BooleanSetting strength = new BooleanSetting("Strength", "strength", true);
+    public final BooleanSetting inventory = new BooleanSetting("InventorySwap", "inventory swap", true);
     public ModeSetting autoSwitch = new ModeSetting("Switch", "The mode that will be used for automatically switching to necessary items.", "Silent", InventoryUtils.SWITCH_MODES);
+
+   private int tick = 0;
 
     @SubscribeEvent
     public void onPlayerUpdate(PlayerUpdateEvent event) {
         if (getNull()) {
             return;
         }
+        tick += 1;
         if (groundCheck.getValue() && !mc.player.isOnGround()) {
             return;
         }
+        if (!(tick >= 10)) {
+            return;
+        } else tick = 0;
+
         if (resistance.getValue() && !(mc.player.hasStatusEffect(StatusEffects.SLOWNESS) || mc.player.hasStatusEffect(StatusEffects.RESISTANCE))) {
             if (onlyWall.getValue()) {
                 if (PhaseModule.isInsideBlock()) {
                     throwPot(POT.RESISTANT, false);
                 }
-            }
-            else if (stp.getValue()) {
-                throwPot(POT.RESISTANT, true);
             }
             else throwPot(POT.RESISTANT, true);
         }
@@ -55,10 +61,6 @@ extends Module {
                     throwPot(POT.STRENGTH, false);
                 }
             }
-            else if (stp.getValue()) {
-                throwPot(POT.STRENGTH, true);
-
-            }
             else throwPot(POT.STRENGTH, true);
         }
 
@@ -67,30 +69,53 @@ extends Module {
     private void throwPot(POT pot, boolean rotate) {
         if (pot == POT.STRENGTH) {
             int prevSlot = mc.player.getInventory().selectedSlot;
-            int slot = InventoryUtils.findPotion(StatusEffects.STRENGTH.value());
+            int slot = InventoryUtils.findPotionInventorySlot(StatusEffects.STRENGTH.value());
+            boolean search = slot > 9;
             if (slot == -1) {
                 return;
             }
             if (rotate) {
                 //我是最好的三元表达式使用者
-                UltraByte.ROTATION_MANAGER.packetRotate(mc.player.getYaw(), 90 );
+                UltraByte.ROTATION_MANAGER.packetRotate(mc.player.getYaw(), 90);
             }
-            InventoryUtils.switchSlot(autoSwitch.getValue(), slot, prevSlot);
+            if (search && inventory.getValue()) {
+                mc.interactionManager.clickSlot(0, slot, 0, SlotActionType.PICKUP, mc.player);
+                mc.interactionManager.clickSlot(0, mc.player.getInventory().selectedSlot + 36, 0, SlotActionType.PICKUP, mc.player);
+                mc.interactionManager.clickSlot(0, slot, 0, SlotActionType.PICKUP, mc.player);
+            } else InventoryUtils.switchSlot(autoSwitch.getValue(), slot, prevSlot);
             NetworkUtils.sendSequencedPacket(sequence -> new PlayerInteractItemC2SPacket(Hand.MAIN_HAND, sequence, mc.player.getYaw(),
                     rotate ? 90 : mc.player.getPitch()));
-            InventoryUtils.switchBack(autoSwitch.getValue(), slot, prevSlot);
+            if (search && inventory.getValue()) {
+                mc.interactionManager.clickSlot(0, slot, 0, SlotActionType.PICKUP, mc.player);
+                mc.interactionManager.clickSlot(0, mc.player.getInventory().selectedSlot + 36, 0, SlotActionType.PICKUP, mc.player);
+                mc.interactionManager.clickSlot(0, slot, 0, SlotActionType.PICKUP, mc.player);
+            }  else InventoryUtils.switchBack(autoSwitch.getValue(), slot, prevSlot);
+
         }
         if (pot == POT.RESISTANT) {
             int prevSlot = mc.player.getInventory().selectedSlot;
-            int slot = InventoryUtils.findPotion(StatusEffects.RESISTANCE.value());
+            int slot = InventoryUtils.findPotionInventorySlot(StatusEffects.RESISTANCE.value());
+            boolean search = slot > 9;
+            if (slot == -1) {
+                return;
+            }
             if (rotate) {
                 //我是最好的三元表达式使用者
                 UltraByte.ROTATION_MANAGER.packetRotate(mc.player.getYaw(), 90);
             }
-            InventoryUtils.switchSlot(autoSwitch.getValue(), slot, prevSlot);
+            if (search && inventory.getValue()) {
+                mc.interactionManager.clickSlot(0, slot, 0, SlotActionType.PICKUP, mc.player);
+                mc.interactionManager.clickSlot(0, mc.player.getInventory().selectedSlot + 36, 0, SlotActionType.PICKUP, mc.player);
+                mc.interactionManager.clickSlot(0, slot, 0, SlotActionType.PICKUP, mc.player);
+            } else InventoryUtils.switchSlot(autoSwitch.getValue(), slot, prevSlot);
             NetworkUtils.sendSequencedPacket(sequence -> new PlayerInteractItemC2SPacket(Hand.MAIN_HAND, sequence, mc.player.getYaw(),
                     rotate ? 90 : mc.player.getPitch()));
-            InventoryUtils.switchBack(autoSwitch.getValue(), slot, prevSlot);
+            if (search && inventory.getValue()) {
+                mc.interactionManager.clickSlot(0, slot, 0, SlotActionType.PICKUP, mc.player);
+                mc.interactionManager.clickSlot(0, mc.player.getInventory().selectedSlot + 36, 0, SlotActionType.PICKUP, mc.player);
+                mc.interactionManager.clickSlot(0, slot, 0, SlotActionType.PICKUP, mc.player);
+            }  else InventoryUtils.switchBack(autoSwitch.getValue(), slot, prevSlot);
+
         }
     }
     enum POT {
